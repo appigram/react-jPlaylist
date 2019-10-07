da31ee301a6e0845a86dc5f34ada02d122057f76
Eugene Sysmanov <appigram@gmail.com>
2019-10-07 14:47:42 +0300

    update deps and bump version

--- src/util/specHelpers/containerSetup.spec.jsx
+++ src/util/specHelpers/containerSetup.spec.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Provider } from 'react-redux'
-import Enzyme, { mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import { createStore, combineReducers } from 'redux'
-import { reducer as jPlayerReducer } from '@appigram/react-jplayer'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createStore, combineReducers } from 'redux';
+import { reducer as jPlayerReducer } from '@appigram/react-jplayer';
 
-import jPlaylistReducer from '../../reducer/reducer'
+import jPlaylistReducer from '../../reducer/reducer';
 
-Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true })
+Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 export default (Container, jPlaylists, jPlayers, props) => {
   const state = {
     jPlayers,
-    jPlaylists
-  }
+    jPlaylists,
+  };
 
   const store = createStore(combineReducers({
     jPlayers: jPlayerReducer,
-    jPlaylists: jPlaylistReducer
-  }), state)
+    jPlaylists: jPlaylistReducer,
+  }), state);
 
   const wrapper = mount(
     <Provider store={store}>
@@ -27,18 +27,18 @@ export default (Container, jPlaylists, jPlayers, props) => {
     </Provider>, {
       context: {
         id: 'TestPlayer',
-        index: 0
+        index: 0,
       },
       childContextTypes: {
         id: PropTypes.string,
-        index: PropTypes.number
-      }
+        index: PropTypes.number,
+      },
     },
-  )
+  );
 
   return {
     wrapper,
     props,
-    store
-  }
-}
+    store,
+  };
+};
